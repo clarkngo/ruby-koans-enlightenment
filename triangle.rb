c20bee71577cbe146947b066d429755f0ec95357
clarkngo <clarkngo@gmail.com>
2018-10-13 16:46:55 -0700

    enlightened in triangle project

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,12 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  if a == b && b == c && a == c
+    :equilateral
+  elsif a == b || b == c || a == c
+    :isosceles
+  else
+    :scalene
 end
 
 # Error class used in part 2.  No need to change this code.
