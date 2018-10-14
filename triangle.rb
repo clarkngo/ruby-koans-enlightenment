42ab0ab2b7f24f8d5ffcdbbeab7c87ce8f449f65
clarkngo <clarkngo@gmail.com>
2018-10-13 17:33:00 -0700

    triangle project refactoring

--- triangle.rb
+++ triangle.rb
@@ -14,18 +14,39 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-
-  # check if triangle
   
+=begin my solution for zero, negative and 
+        sum of any two sides must be more than third side
+  # check if triangle
+  if a > c
+    temp = c
+    c = a
+    a = temp
+  end
+  if b > c 
+    temp = c
+    c = b
+    b = temp
+  end 
+
   # if zero or negative length
-  if a < 1 || b < 1 || c < 1
+  if a <= 0 || b <= 0 || c <= 0 || a + b <= c
     raise TriangleError
-  elsif a + b < c
+  end
+=end
+
+
+  s = (a + b + c) / 2.0
+  
+  # the following must be positive to be a valid triangle
+  ok = (s - a) * (s - b) * (s - c)
+  
+  if a <= 0 || b <= 0 || c <= 0 || ok <= 0 then 
     raise TriangleError
   end
 
   # type of triangles
-  if a == b && b == c && a == c
+  if a == b && b == c 
     :equilateral
   elsif a == b || b == c || a == c
     :isosceles
